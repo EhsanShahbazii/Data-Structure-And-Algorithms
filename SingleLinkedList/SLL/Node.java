b62c15f1e3ead6e67588d89bb4afc5162d8a7b1d
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-13 20:22:45 +0330

    Create Node.java
    add node class

---
+++ SingleLinkedList/SLL/Node.java
@@ -0,0 +1,31 @@
+public class Node<Type> {
+
+    private Type m_value;
+    private Node<Type> m_next;
+
+    public Node() {
+        m_value = null;
+        m_next = null;
+    }
+
+    public Node(Type value, Node<Type> next) {
+        m_value = value;
+        m_next = next;
+    }
+
+    public Type getValue() {
+        return m_value;
+    }
+
+    public Node<Type> getNext() {
+        return m_next;
+    }
+
+    public void setValue(Type value) {
+        m_value = value;
+    }
+
+    public void setNext(Node<Type> next) {
+        m_next = next;
+    }
+}
