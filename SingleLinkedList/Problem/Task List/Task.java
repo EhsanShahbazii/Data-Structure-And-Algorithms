e271687c54fbeaff422eec6fb483f74d893f7f44
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-20 23:56:44 +0330

    add Task classes

---
+++ SingleLinkedList/Problem/Task List/Task.java
@@ -0,0 +1,13 @@
+package com.tabrizu.ds;
+
+public class Task {
+    String m_title;
+
+    public Task(String title){
+        m_title = title;
+    }
+
+    public String getTitle(){
+        return m_title;
+    }
+}
