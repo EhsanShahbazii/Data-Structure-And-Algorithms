df35f1d98740281434a987759852566af2b8300a
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-11-27 12:17:20 +0330

    update

--- DoubleLinkedList/Java/Node.java
+++ DoubleLinkedList/Java/Node.java
@@ -1,13 +1,13 @@
 public class Node<Type> {
 
     private Type m_value;
-    private Node<Type> m_next;
     private Node<Type> m_prev;
+    private Node<Type> m_next;
 
     public Node() {
         m_value = null;
-        m_next = null;
         m_prev = null;
+        m_next = null;
     }
 
     public Node(Type value, Node<Type> prev, Node<Type> next) {
@@ -20,23 +20,25 @@ public class Node<Type> {
         return m_value;
     }
 
-    public Node<Type> getNext() {
-        return m_next;
-    }
-
     public Node<Type> getPrev() {
         return m_prev;
+    }
+
+    public Node<Type> getNext() {
+        return m_next;
     }
 
     public void setValue(Type value) {
         m_value = value;
     }
 
-    public void setNext(Node<Type> next) {
-        m_next = next;
-    }
-
     public void setPrev(Node<Type> prev) {
         m_prev = prev;
     }
+
+    public void setNext(Node<Type> next) {
+        m_next = next;
+    }
 }
+
+// update 27 November
