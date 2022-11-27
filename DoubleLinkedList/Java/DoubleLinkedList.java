655cdd3ef7e00688e7fa80b0a367b7dec44ba5f8
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-11-27 12:16:01 +0330

    update

--- DoubleLinkedList/Java/DoubleLinkedList.java
+++ DoubleLinkedList/Java/DoubleLinkedList.java
@@ -11,28 +11,37 @@ public class DoubleLinkedList<Type> implements IDoubleLinkedList<Type> {
     }
 
     @Override
-    public void insertFirst(Type value) {
-
-        m_head = new Node<Type>(value, null, m_head);
-
-        m_tail = (m_size == 0) ? m_head : m_tail;
-
+    public void addFirst(Type value) {
+        m_head = new Node<>(value, null, m_head);
+        m_tail = m_size == 0 ? m_head : m_tail;
         m_size++;
     }
 
     @Override
-    public void insertLast(Type value) {
-
-        m_tail = new Node<Type>(value, m_tail, null);
-
-        m_head = (m_size == 0) ? m_tail : m_head;
-
+    public void addLast(Type value) {
+        m_tail = new Node<>(value, m_tail, null);
+        m_head = m_size == 0 ? m_tail : m_head;
         m_size++;
     }
 
     @Override
-    public boolean isEmpty() {
-        return m_size == 0;
+    public Type removeFirst() {
+        Type prevHead = m_head.getValue();
+        m_head = m_head.getNext();
+        m_head.setPrev(null);
+        m_size--;
+
+        return prevHead;
+    }
+
+    @Override
+    public Type removeLast() {
+        Type prevTail = m_tail.getValue();
+        m_tail = m_tail.getPrev();
+        m_tail.setNext(null);
+        m_size--;
+
+        return prevTail;
     }
 
     @Override
@@ -41,51 +50,29 @@ public class DoubleLinkedList<Type> implements IDoubleLinkedList<Type> {
     }
 
     @Override
-    public Node<Type> first() {
-        return m_head;
-    }
-
-    @Override
-    public Node<Type> last() {
-        return m_tail;
-    }
-
-    @Override
-    public Type deleteFirst() {
-
-        if (m_size == 0)
-            return null;
-
-        Type lastHead = m_head.getValue();
-        m_head = m_head.getNext();
-        m_head.setPrev(null);
-        m_size--;
-
-        return lastHead;
-    }
-
-    @Override
-    public Type deleteLast() {
-
-        if (m_size == 0)
-            return null;
-
-        Type lastTail = m_tail.getValue();
-        m_tail = m_tail.getPrev();
-        m_tail.setNext(null);
-        m_size--;
-
-        return lastTail;
+    public boolean isEmpty() {
+        return m_size == 0;
     }
 
     @Override
     public void showList() {
+        Node<Type> pointer = m_head;
 
-        System.out.println(m_head.getValue());
-
-        for (int i = 1; i < m_size; i++) {
-            System.out.println(m_head.getNext().getValue());
-            m_head = m_head.getNext();
+        for (int i = 0; i < m_size; i++) {
+            System.out.println(pointer.getValue());
+            pointer = pointer.getNext();
         }
     }
+
+    @Override
+    public Node<Type> getHead() {
+        return m_head;
+    }
+
+    @Override
+    public Node<Type> getTail() {
+        return m_tail;
+    }
 }
+
+// update 27 November
