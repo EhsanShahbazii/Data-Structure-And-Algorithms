bf98b3552738d598974d32258f95555d68aa4e5c
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-11-27 12:04:04 +0330

    update

--- SingleLinkedList/Java/SingleLinkedList.java
+++ SingleLinkedList/Java/SingleLinkedList.java
@@ -11,28 +11,50 @@ public class SingleLinkedList<Type> implements ISingleLinkedList<Type> {
     }
 
     @Override
-    public void insertFirst(Type value) {
-
-        m_head = new Node<Type>(value, m_head);
-
-        m_tail = (m_size == 0) ? m_head : m_tail;
-
+    public void addFirst(Type value) {
+        m_head = new Node<>(value, m_head);
+        m_tail = m_size == 0 ? m_head : m_tail;
         m_size++;
     }
 
     @Override
-    public void insertLast(Type value) {
-
-        m_tail = new Node<Type>(value, m_tail);
-
-        m_head = (m_size == 0) ? m_tail : m_head;
-
+    public void addLast(Type value) {
+        m_tail = new Node<>(value, null);
+        m_head = m_size == 0 ? m_tail : m_head;
         m_size++;
     }
 
     @Override
-    public boolean isEmpty() {
-        return m_size == 0;
+    public Type removeFirst() {
+        Type prevHead = m_head.getValue();
+        m_head = m_head.getNext();
+        m_size--;
+
+        return prevHead;
+    }
+
+    @Override
+    public Type removeLast() {
+
+        Type prevTail;
+        prevTail = m_size != 0 ? m_tail.getValue() : null;
+
+        if (m_size == 0) {
+            return null;
+        } else if (m_size == 1) {
+            return removeFirst();
+        } else {
+            Node<Type> pointer = m_head;
+
+            for (int i = 1; i < m_size - 1; i++) {
+                pointer = pointer.getNext();
+            }
+
+            pointer.setNext(null);
+            m_tail = pointer;
+            m_size--;
+        }
+        return prevTail;
     }
 
     @Override
@@ -41,25 +63,29 @@ public class SingleLinkedList<Type> implements ISingleLinkedList<Type> {
     }
 
     @Override
-    public Node<Type> first() {
+    public boolean isEmpty() {
+        return m_size == 0;
+    }
+
+    @Override
+    public void showList() {
+        Node<Type> pointer = m_head;
+
+        for (int i = 0; i < m_size; i++) {
+            System.out.println(pointer.getValue());
+            pointer = pointer.getNext();
+        }
+    }
+
+    @Override
+    public Node<Type> getHead() {
         return m_head;
     }
 
     @Override
-    public Node<Type> last() {
+    public Node<Type> getTail() {
         return m_tail;
     }
-
-    @Override
-    public Type deleteFirst() {
-
-        if (m_size == 0)
-            return null;
-
-        Type lastHead = m_head.getValue();
-        m_head = m_head.getNext();
-        m_size--;
-
-        return lastHead;
-    }
 }
+
+// update 27 November
