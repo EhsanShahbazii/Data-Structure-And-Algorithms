3ba4f63fc1de9386d6067145f4dc67473baef48d
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-14 21:42:35 +0330

    Create reverse.java

---
+++ Chapter6/Stacks, Queues, and Deques/reverse.java
@@ -0,0 +1,17 @@
+/*
+Reversing an Array Using a Stack
+
+last update 2022 Dec 14
+*/
+
+// A generic method for reversing an array.
+public static <E> void reverse(E[] array) {
+  
+  Stack<E> buffer = new ArrayStack<>(a.length);
+  
+  for (int i = 0; i < array.length; i++)
+    buffer.push(a[i]);
+  for (int i = 0; i < array.length; i++)
+    a[i] = buffer.pop();
+  
+}
