effaac7ea084f3ad233157a029f93788e8eb0c96
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-14 21:40:31 +0330

    Create LinkedStack.java

---
+++ Chapter6/Stacks, Queues, and Deques/LinkedStack/LinkedStack.java
@@ -0,0 +1,34 @@
+/*
+Implementing a Stack with a Singly Linked List
+
+last update 2022 Dec 14
+*/
+public class LinkedStack<E> implements Stack<E> {
+  
+  private SinglyLinkedList<E> list = new SinglyLinkedList<>(); // an empty list
+  
+  // new stack relies on the initially empty list
+  public LinkedStack() {
+  } 
+  
+  public int size() {
+    return list.size();
+  }
+  
+  public boolean isEmpty() { 
+    return list.isEmpty();
+  }
+  
+  public void push(E element) {
+    list.addFirst(element);
+  }
+  
+  public E top() { 
+    return list.first();
+  }
+  
+  public E pop() {
+    return list.removeFirst();
+  }
+  
+}
