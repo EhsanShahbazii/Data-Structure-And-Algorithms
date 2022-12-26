3566f6db8de3db9ba5df36c8b4ef32d544b69527
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-26 17:53:16 +0330

    Update IStack.java

--- Chapter6/Stacks, Queues, and Deques/Stack/IStack.java
+++ Chapter6/Stacks, Queues, and Deques/Stack/IStack.java
@@ -9,13 +9,13 @@ public interface Stack<E> {
   * Returns the number of elements in the stack.
   * @return number of elements in the stack
   */
-  int size();
+  int size( );
   
   /**
   * Tests whether the stack is empty.
   * @return true if the stack is empty, false otherwise
   */
-  boolean isEmpty();
+  boolean isEmpty( );
   
   /**
   * Inserts an element at the top of the stack
@@ -27,12 +27,12 @@ public interface Stack<E> {
   * Returns, but does not remove, the element at the top of the stack
   * @return top element in the stack (or null if empty)
   */
-  E top();
+  E top( );
   
   /**
   * Removes and returns the top element from the stack
   * @return element removed (or null if empty)
   */
-  E pop();
+  E pop( );
   
 }
