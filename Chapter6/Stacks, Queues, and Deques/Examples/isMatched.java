04d5337a9e8c3de9f4ebce4fb9de1c640abcc2fc
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-26 17:52:02 +0330

    Update isMatched.java

--- Chapter6/Stacks, Queues, and Deques/Examples/isMatched.java
+++ Chapter6/Stacks, Queues, and Deques/Examples/isMatched.java
@@ -29,18 +29,18 @@ public static boolean isMatched(String expression) {
   final String opening = "({["; // opening delimiters
   final String closing = ")}]"; // respective closing delimiters
   
-  Stack<Character> buffer = new LinkedStack<>();
+  Stack<Character> buffer = new LinkedStack<>( );
   
-  for (char c : expression.toCharArray()) {
+  for (char c : expression.toCharArray( )) {
     if (opening.indexOf(c) != −1) // this is a left delimiter
       buffer.push(c);
     else if (closing.indexOf(c) != −1) { // this is a right delimiter
-      if (buffer.isEmpty()) // nothing to match with
+      if (buffer.isEmpty( )) // nothing to match with
         return false;
-      if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+      if (closing.indexOf(c) != opening.indexOf(buffer.pop( )))
         return false; // mismatched delimiter
     }
   }
-  return buffer.isEmpty(); // were all opening delimiters matched?
+  return buffer.isEmpty( ); // were all opening delimiters matched?
   
 }
