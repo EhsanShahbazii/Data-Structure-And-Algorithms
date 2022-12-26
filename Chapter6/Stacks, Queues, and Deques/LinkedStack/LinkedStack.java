218f0442fc0d3a6cb0a50b33550ab869037f3ece
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-26 17:52:30 +0330

    Update LinkedStack.java

--- Chapter6/Stacks, Queues, and Deques/LinkedStack/LinkedStack.java
+++ Chapter6/Stacks, Queues, and Deques/LinkedStack/LinkedStack.java
@@ -5,30 +5,30 @@ last update 2022 Dec 14
 */
 public class LinkedStack<E> implements Stack<E> {
   
-  private SinglyLinkedList<E> list = new SinglyLinkedList<>(); // an empty list
+  private SinglyLinkedList<E> list = new SinglyLinkedList<>( ); // an empty list
   
   // new stack relies on the initially empty list
-  public LinkedStack() {
+  public LinkedStack( ) {
   } 
   
-  public int size() {
-    return list.size();
+  public int size( ) {
+    return list.size( );
   }
   
-  public boolean isEmpty() { 
-    return list.isEmpty();
+  public boolean isEmpty( ) { 
+    return list.isEmpty( );
   }
   
   public void push(E element) {
     list.addFirst(element);
   }
   
-  public E top() { 
-    return list.first();
+  public E top( ) { 
+    return list.first( );
   }
   
-  public E pop() {
-    return list.removeFirst();
+  public E pop( ) {
+    return list.removeFirst( );
   }
   
 }
