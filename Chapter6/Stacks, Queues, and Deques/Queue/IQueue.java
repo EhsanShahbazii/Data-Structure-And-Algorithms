8e58f2f42af347ccb1ea7860f269c198711f5948
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2022-12-26 18:02:23 +0330

    Update IQueue.java

--- Chapter6/Stacks, Queues, and Deques/Queue/IQueue.java
+++ Chapter6/Stacks, Queues, and Deques/Queue/IQueue.java
@@ -1,18 +1,18 @@
 public interface IQueue<E> {
   
-  /** Returns the number of elements in the queue. */
+  // Returns the number of elements in the queue.
   int size( );
   
-  /** Tests whether the queue is empty. */
+  // Tests whether the queue is empty. 
   boolean isEmpty( );
   
-  /** Inserts an element at the rear of the queue. */
+  // Inserts an element at the rear of the queue. 
   void enqueue(E e);
   
-  /** Returns, but does not remove, the first element of the queue (null if empty). */
+  // Returns, but does not remove, the first element of the queue (null if empty).
   E first( );
   
-  /** Removes and returns the first element of the queue (null if empty). */
+  // Removes and returns the first element of the queue (null if empty). 
   E dequeue( );
   
 }
